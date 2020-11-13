2765972d2ae38cc9bcb8398989146aa01f312d11
Sam <hatem@clearrain.xyz>
2020-11-12 20:05:53 -0500

    update migrations for new contract

--- migrations/10_all.js
+++ migrations/10_all.js
@@ -1,49 +1,49 @@
-const TestToken = artifacts.require('TestToken');
+/* globals artifacts, web3 */
+
 const Subscription = artifacts.require('Subscription')
-const deployFramework = require("@superfluid-finance/ethereum-contracts/scripts/deploy-framework");
-const deployTestToken = require("@superfluid-finance/ethereum-contracts/scripts/deploy-test-token");
-const deploySuperToken = require("@superfluid-finance/ethereum-contracts/scripts/deploy-super-token");
-const SuperfluidSDK = require("@superfluid-finance/ethereum-contracts");
+const deployFramework = require('@superfluid-finance/ethereum-contracts/scripts/deploy-framework')
+const deployTestToken = require('@superfluid-finance/ethereum-contracts/scripts/deploy-test-token')
+const deploySuperToken = require('@superfluid-finance/ethereum-contracts/scripts/deploy-super-token')
+const SuperfluidSDK = require('@superfluid-finance/ethereum-contracts')
 
 const MINIMUM_FLOW_RATE = 1929012345679
 
-async function setupLocalSuperfluid(deployer) {
-  global.web3 = web3;
+async function setupLocalSuperfluid (deployer) {
+  global.web3 = web3
 
-  const errorHandler = err => { if (err) throw err; };
+  const errorHandler = err => { if (err) throw err }
 
-  await deployFramework(errorHandler);
+  await deployFramework(errorHandler)
 
-  let sf = new SuperfluidSDK.Framework({ web3Provider: web3.currentProvider });
-  await sf.initialize();
+  const sf = new SuperfluidSDK.Framework({ web3Provider: web3.currentProvider })
+  await sf.initialize()
 
-  await deployTestToken(errorHandler, [":", "fDAI"]);
-  await deploySuperToken(errorHandler, [":", "fDAI"]);
+  await deployTestToken(errorHandler, [':', 'fDAI'])
+  await deploySuperToken(errorHandler, [':', 'fDAI'])
 
-  return sf;
+  return sf
 }
 
 module.exports = async function (deployer, _, accounts) {
   const alice = accounts[0]
 
-  let sf;
+  let sf
   if ((await web3.eth.net.getId()) === 5 /* goerli */) {
-      sf = new SuperfluidSDK.Framework({
-        chainId: 5,
-        version: process.env.SUPERFLUID_VERSION,
-        web3Provider: web3.currentProvider
-      })
-      await sf.initialize();
-      await deployJAMMToken(deployer)
+    sf = new SuperfluidSDK.Framework({
+      chainId: 5,
+      version: process.env.SUPERFLUID_VERSION,
+      web3Provider: web3.currentProvider
+    })
+    await sf.initialize()
   } else {
-      console.log("Using local superfluid");
-      sf = await setupLocalSuperfluid(deployer);
+    console.log('Using local superfluid')
+    sf = await setupLocalSuperfluid(deployer)
   }
 
-  const daiAddress = await sf.resolver.get("tokens.fDAI");
-  const dai = await sf.contracts.TestToken.at(daiAddress);
+  const daiAddress = await sf.resolver.get('tokens.fDAI')
+  const dai = await sf.contracts.TestToken.at(daiAddress)
 
-  const MINT_AMOUNT = web3.utils.toWei("20000000", "ether")
+  const MINT_AMOUNT = web3.utils.toWei('20000000', 'ether')
 
   await dai.mint(alice, MINT_AMOUNT, { from: alice })
 
--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
-const Migrations = artifacts.require("Migrations");
+/* globals artifacts */
 
-module.exports = function(deployer) {
-  deployer.deploy(Migrations);
-};
+const Migrations = artifacts.require('Migrations')
+
+module.exports = function (deployer) {
+  deployer.deploy(Migrations)
+}
